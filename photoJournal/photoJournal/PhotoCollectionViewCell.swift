e14d6613dbd1665fd7a2d0db0853986eb000f5e8
Sam <samroman3@gmail.com>
2019-09-29 13:08:26 -0400

    Adds Custom collectionviewcell and corresponding storyboard outlets

---
+++ photoJournal/photoJournal/PhotoCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoCollectionViewCell.swift
+//  photoJournal
+//
+//  Created by Sam Roman on 9/29/19.
+//  Copyright © 2019 Sam Roman. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImag: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var date: UILabel!
+    
+    @IBOutlet weak var photoDescription: UITextView!
+    
+    
+}
